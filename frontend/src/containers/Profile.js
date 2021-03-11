5972a4129b45083eac7149719a793b1e3fe1dd50
Shaylan Dias <shaylandias@berkeley.edu>
2021-03-10 20:03:04 -0800

    some sample data and updated market api call

--- frontend/src/containers/Profile.js
+++ frontend/src/containers/Profile.js
@@ -10,12 +10,49 @@ import LoaderButton from "../components/LoaderButton";
 
 export default function Profile() {
 
+  let data = [{
+    name: "Tesla",
+    symbol: "TSLA",
+    bid: "The Moon",
+  },
+  {
+    name: "Apple",
+    symbol: "AAPL",
+    bid: 123,
+  },
+  {
+    name: "Microsoft",
+    symbol: "MSFT",
+    bid: 235,
+  },
+  {
+    name: "PayPal",
+    symbol: "PYPL",
+    bid: 189,
+  },]
+
   return (
     <div className="Profile">
       <div className="">
         <h1>Stonks</h1>
         <p className="">Welcome to your profile!</p>
       </div>
+      <Table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Symbol</th>
+            <th>Bid</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map(item => {
+            return <tr>
+
+            </tr>
+          })}
+        </tbody>
+      </Table>
     </div>
   );
 }
